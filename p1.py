4f86f83093bff5ce3bd607f1efde88fce8d62376
Wesley Lin <wesley.lin@vanderbilt.edu>
2016-11-29 19:32:55 -0600

    problem 1 and 2

---
+++ p1.py
@@ -0,0 +1,5 @@
+def is_prime(n):
+    for i in range(n)
+
+for i in range(2017):
+    print(i)
---
+++ p2.py
@@ -0,0 +1,7 @@
+for j in range(5):
+    for i in range(10):
+        print("|_/*\\_",end="")
+    print("|")
+    for i in range(10):
+        print("/*_|_\\",end="")
+    print("*")
